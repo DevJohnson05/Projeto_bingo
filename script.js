39f34bae0fe3c3ed610accebfc7e053bfd6d5b61
Vinícius Johnson <dev.vinicius.j@gmail.com>
2025-03-08 15:35:30 -0400

    Atualização da pagina dos núneros sorteados e ajustes nos scripts de javascript

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ function gerarnumero() {
     fetch('http://localhost:8000/bingo.php') // Fetch API é usada para fazer requisições HTTP
         .then(response => response.json())
         .then(data => {
-
             document.getElementById('numero').innerHTML = data.numero;
             document.getElementById('letra').innerHTML = data.letra;
+            salvarNumero(data.numero, data.letra); // Chama a função salvarNumero após obter os dados
         })
-        .catch(error => console.error("Erro ao obter número" , error));
+        .catch(error => console.error("Erro ao obter número", error));
+}
+
+function salvarNumero(numero, letra) {
+    let numerosSorteados = JSON.parse(localStorage.getItem('numerosSorteados')) || [];
+    let numeroCompleto = letra + numero; // Combina letra e número
+
+    if (!numerosSorteados.includes(numeroCompleto)) {
+        numerosSorteados.push(numeroCompleto);
+        localStorage.setItem('numerosSorteados', JSON.stringify(numerosSorteados));
+        document.getElementById('numeros').innerHTML = numerosSorteados.join(', ');
+    }
 }
